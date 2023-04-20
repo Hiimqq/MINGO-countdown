b7e91d8c1e2746774e483aeb3e0152b74d63922b
Alex Carmody <alex-carmody@live.com>
2023-04-20 00:39:22 -0400

    Prevent form submission from refreshing page

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Minimum with Today's Date and No Specific Time
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Takes Values From Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    console.log(e);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
